02603420c2006984d9e7f23347687f0a3cc6cf33
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2019-03-19 20:59:42 +0530

    Create checkEvenOdd.py

---
+++ checkEvenOdd.py
@@ -0,0 +1,17 @@
+class number:
+    
+    def check(self,value=None):
+        self.value=value
+        if(self.value%2==0):
+            print("Even")
+        else:
+            print("Odd")
+try:  
+    inp=int(input())
+    obj=number()
+    obj.check(inp)
+except ValueError:
+    print("Invalid Input")
+
+
+        
