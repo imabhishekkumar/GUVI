201fae8c84262373e59599b1f60c66347a0191e7
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2019-07-09 13:33:19 +0530

    pro set1q1

--- pro/set_1/1.py
+++ pro/set_1/1.py
@@ -2,7 +2,7 @@ def checkLongest(str1,str2):
         if(str1 in str2):
             return str1
         else:
-            return checkLongest(str1[0:len(str1)-2],str2)
+            return checkLongest(str1[0:len(str1)-1],str2)
         
 
 inp = int(input())
