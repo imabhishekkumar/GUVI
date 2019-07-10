24b03ec2876a4da71d993fc2ce974ddd973474e0
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2019-07-10 08:38:53 +0530

    added pro-set1.3

--- pro/set_1/3.py
+++ pro/set_1/3.py
@@ -2,9 +2,17 @@ a,b = input().split()
 lenA = len(a)
 lenB = len(b)
 cost=0
-for i in range(0,lenA):
-    if a[i] != b[i]:
-        cost+=1
+if(lenA<lenB):
+    for i in range(0,lenA):
+        if a[i] != b[i]:
+            cost+=1
+    cost+=(lenB-lenA)
+else:
+    for i in range(0,lenB):
+        if b[i] != a[i]:
+            cost+=1
+    cost+=(lenA-lenB)
+
     
-cost+=(lenB-lenA)
+
 print(cost)
